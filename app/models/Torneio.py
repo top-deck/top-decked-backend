0567a01d61c0d27b0b18501e8aa4d4a02cc91763
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-08-08 09:29:04 -0300

    refact: TorneioBase

--- app/models/Torneio.py
+++ app/models/Torneio.py
@@ -6,16 +6,16 @@ from app.models.Jogador import JogadorPublicoLoja
 
 
 class TorneioBase(SQLModel):
-    id: Optional[str] = Field(default=None, primary_key=True)
     cidade: str = Field(index=True)
     estado: Optional[str] = Field(default=None, index=True, nullable=True)
     tempo_por_rodada: int = Field(default=30, index=True)
     data_inicio: date = Field(default=None)
     finalizado: bool = Field(default=False)
-    loja_id: int = Field(foreign_key="loja.id", nullable=True)
     
-    
+
 class Torneio(TorneioBase, table=True):
+    id: Optional[str] = Field(default=None, primary_key=True)
+    loja_id: int = Field(foreign_key="loja.id", nullable=True)
     loja: Optional[Loja] = Relationship(sa_relationship_kwargs={"lazy": "joined"})
     
 
