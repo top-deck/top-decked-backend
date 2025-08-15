ffbbd7a641739d7482dec8071749d5cad931882f
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-08-15 03:38:43 -0300

    feat: atualizar tipo jogador

--- app/schemas/TipoJogador.py
+++ app/schemas/TipoJogador.py
@@ -3,5 +3,15 @@ from typing import Optional
 from app.models import TipoJogadorBase
 
 
+class TipoJogadorAtualizar(TipoJogadorBase):
+    nome: str | None = None
+    pt_vitoria: float | None = None
+    pt_derrota: float | None = None
+    pt_empate: float | None = None
+    pt_oponente_perde: float | None = None
+    pt_oponente_ganha: float | None = None
+    pt_oponente_empate: float | None = None
+    tcg: str | None = None
+
 class TipoJogadorPublico(TipoJogadorBase):
     id: int
