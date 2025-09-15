e2b893f426d24b7681ccaa148eea6597221fefec
Andre Felipe Almeida <116607039+AndreFelipeAlmeida@users.noreply.github.com>
2025-09-15 19:15:34 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,14 +24,9 @@ UPLOAD_DIR = "app/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True) 
 app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
